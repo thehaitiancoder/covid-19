bb7dfcde071946b87561eb7948e80324b14d6e43
RalphGustave <rdgustave@gmail.com>
2020-05-21 20:23:53 -0700

    latest cases

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -10,49 +10,49 @@ export class HomeComponent implements OnInit {
     regions: Regions = {
         sud: {
             totalTests: 16,
-            confirmedCases: 5,
+            confirmedCases: 8,
             death: 0,
             deathPercentage: 0,
         },
         grand_Anse: {
             totalTests: 9,
-            confirmedCases: 2,
+            confirmedCases: 4,
             death: 0,
             deathPercentage: 0,
         },
         nippes: {
             totalTests: 30,
-            confirmedCases: 7,
+            confirmedCases: 10,
             death: 0,
             deathPercentage: 0,
         },
         sud_Est: {
             totalTests: 36,
-            confirmedCases: 14,
+            confirmedCases: 18,
             death: 1,
             deathPercentage: 14.3,
         },
         ouest: {
             totalTests: 755,
-            confirmedCases: 445,
-            death: 13,
+            confirmedCases: 567,
+            death: 16,
             deathPercentage: 5.6,
         },
         centre: {
             totalTests: 50,
-            confirmedCases: 13,
+            confirmedCases: 14,
             death: 0,
             deathPercentage: 0,
         },
         artibonite: {
             totalTests: 72,
-            confirmedCases: 60,
+            confirmedCases: 62,
             death: 5,
             deathPercentage: 15.4,
         },
         nord_Est: {
             totalTests: 59,
-            confirmedCases: 18,
+            confirmedCases: 19,
             death: 2,
             deathPercentage: 16.7,
         },
@@ -69,9 +69,9 @@ export class HomeComponent implements OnInit {
             deathPercentage: 0,
         },
     };
-    aggrTotalTests: Number = 2270;
-    aggrConfirmedCases: Number = 596;
-    aggrTotalDeaths: Number = 22;
+    aggrTotalTests: Number = 2519;
+    aggrConfirmedCases: Number = 734;
+    aggrTotalDeaths: Number = 25;
     aggrTotalRecoved: Number = 21;
     allTooltips: Array<String> = [];
 

52e0a2c80fe895cfcc01e712f3fbbc0a79948fbc
RalphGustave <rdgustave@gmail.com>
2020-07-10 14:17:46 -0700

    latest cases

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -10,69 +10,69 @@ export class HomeComponent implements OnInit {
     regions: Regions = {
         sud: {
             totalTests: 16,
-            confirmedCases: 161,
+            confirmedCases: 173,
             death: 5,
             deathPercentage: 0,
         },
         grand_Anse: {
             totalTests: 9,
-            confirmedCases: 126,
-            death: 9,
+            confirmedCases: 133,
+            death: 10,
             deathPercentage: 0,
         },
         nippes: {
             totalTests: 30,
-            confirmedCases: 92,
+            confirmedCases: 101,
             death: 3,
             deathPercentage: 0,
         },
         sud_Est: {
             totalTests: 36,
-            confirmedCases: 133,
-            death: 2,
+            confirmedCases: 158,
+            death: 6,
             deathPercentage: 14.3,
         },
         ouest: {
             totalTests: 755,
-            confirmedCases: 4584,
-            death: 54,
+            confirmedCases: 4882,
+            death: 56,
             deathPercentage: 5.6,
         },
         centre: {
             totalTests: 50,
-            confirmedCases: 267,
-            death: 0,
+            confirmedCases: 298,
+            death: 3,
             deathPercentage: 0,
         },
         artibonite: {
             totalTests: 72,
-            confirmedCases: 305,
-            death: 15,
+            confirmedCases: 342,
+            death: 24,
             deathPercentage: 15.4,
         },
         nord_Est: {
             totalTests: 59,
-            confirmedCases: 97,
-            death: 4,
+            confirmedCases: 109,
+            death: 5,
             deathPercentage: 16.7,
         },
         nord: {
             totalTests: 50,
-            confirmedCases: 212,
+            confirmedCases: 248,
             death: 12,
             deathPercentage: 12.5,
         },
         nord_Ouest: {
             totalTests: 14,
-            confirmedCases: 124,
+            confirmedCases: 138,
             death: 6,
             deathPercentage: 0,
         },
     };
-    aggrTotalTests: Number = 12735;
-    aggrConfirmedCases: Number = 6101;
-    aggrTotalDeaths: Number = 110;
-    aggrTotalRecoved: Number = 1141;
+    aggrTotalTests: Number = 14325;
+    aggrConfirmedCases: Number = 6582;
+    aggrTotalDeaths: Number = 130;
+    aggrTotalRecoved: Number = 2459;
     allTooltips: Array<String> = [];
 
     constructor() {}

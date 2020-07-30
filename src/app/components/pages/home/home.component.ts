bd98e6dee8c204ac1cafc38c7736c4267e21305f
RalphGustave <rdgustave@gmail.com>
2020-07-29 22:13:40 -0700

    latest cases

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -10,69 +10,69 @@ export class HomeComponent implements OnInit {
     regions: Regions = {
         sud: {
             totalTests: 16,
-            confirmedCases: 200,
-            death: 5,
+            confirmedCases: 205,
+            death: 6,
             deathPercentage: 0,
         },
         grand_Anse: {
             totalTests: 9,
             confirmedCases: 149,
-            death: 11,
+            death: 12,
             deathPercentage: 0,
         },
         nippes: {
             totalTests: 30,
-            confirmedCases: 119,
+            confirmedCases: 120,
             death: 4,
             deathPercentage: 0,
         },
         sud_Est: {
             totalTests: 36,
-            confirmedCases: 206,
+            confirmedCases: 209,
             death: 7,
             deathPercentage: 14.3,
         },
         ouest: {
             totalTests: 755,
-            confirmedCases: 5191,
-            death: 60,
+            confirmedCases: 5259,
+            death: 61,
             deathPercentage: 5.6,
         },
         centre: {
             totalTests: 50,
-            confirmedCases: 351,
+            confirmedCases: 355,
             death: 3,
             deathPercentage: 0,
         },
         artibonite: {
             totalTests: 72,
-            confirmedCases: 404,
+            confirmedCases: 415,
             death: 31,
             deathPercentage: 15.4,
         },
         nord_Est: {
             totalTests: 59,
-            confirmedCases: 153,
+            confirmedCases: 163,
             death: 5,
             deathPercentage: 16.7,
         },
         nord: {
             totalTests: 50,
-            confirmedCases: 327,
+            confirmedCases: 339,
             death: 20,
             deathPercentage: 12.5,
         },
         nord_Ouest: {
             totalTests: 14,
-            confirmedCases: 160,
+            confirmedCases: 164,
             death: 10,
             deathPercentage: 0,
         },
     };
-    aggrTotalTests: Number = 17202;
-    aggrConfirmedCases: Number = 7260;
-    aggrTotalDeaths: Number = 156;
-    aggrTotalRecoved: Number = 4236;
+    aggrTotalTests: Number = 17951;
+    aggrConfirmedCases: Number = 7378;
+    aggrTotalDeaths: Number = 159;
+    aggrTotalRecoved: Number = 4467;
     allTooltips: Array<String> = [];
 
     constructor() {}
